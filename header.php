b56c211bc3e54c6b3224d7c9ce790aa0c430c93b
AlexsanLaptev <43809083+AlexsanLaptev@users.noreply.github.com>
2021-05-31 13:31:13 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Мяндекс.Муррркет - магазин</title>
+    <link rel="icon" href="img/Fevicon.png" type="image/png">
+  <link rel="stylesheet" href="bibl/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/main.css">
+  <script src="js/jquery-3.6.0.min.js"></script>
+</head>
+<body class = "sent">
+    <header>
+    <h1>Мурмаркет</h1>
+    <nav><a href="index.php">Товар</a><a href="about.php" onclick="cor();">О Нас</a><a href="basket.php" onclick="cor();">Корзина</a></nav>
+</header>
