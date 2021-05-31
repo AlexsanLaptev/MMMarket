dedfd7e3020597a2875d29361d2e82950ae0e8bf
AlexsanLaptev <43809083+AlexsanLaptev@users.noreply.github.com>
2021-05-31 13:30:40 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="py-3">
+    <div class="copy-bottom-txt text-center py-3">
+        <p> 
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> Все права защищены
+        </p>
+    </div>
+    <div class="social-icons mt-lg-3 mt-2 text-center">
+        <ul>
+            <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+            <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+            <li><a href="#"><span class="fa fa-rss"></span></a></li>
+        </ul>
+    </div>
+</footer>
+
+<script src="https://use.fontawesome.com/df966d76e1.js"></script>
+
+</body>
+</html>
